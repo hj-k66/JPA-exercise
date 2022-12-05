c62dfe974b4fe7cfe634ca65aa87e69387a5dc5d
heejeong <hjx66@naver.com>
2022-12-05 15:10:34 +0900

    feat: AUTHORIZATION에서 토큰 꺼내고 유효한지 check

--- src/main/java/com/jpa/jpaexercise/configuration/JwtTokenFilter.java
+++ src/main/java/com/jpa/jpaexercise/configuration/JwtTokenFilter.java
@@ -1,7 +1,10 @@
 package com.jpa.jpaexercise.configuration;
 
 import com.jpa.jpaexercise.hospital.service.UserService;
+import com.jpa.jpaexercise.utils.JwtTokenUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +20,7 @@ import java.util.List;
 
 //OncePerRequestFilter:들어갈때마다 체킹한다.
 @RequiredArgsConstructor
+@Slf4j
 public class JwtTokenFilter extends OncePerRequestFilter {
     private final UserService userService;
     private final String secretKey;
@@ -24,8 +28,36 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //권한 여부 결정
+        //권한 부여 X case
+        //1. Token X —> Request할 때 Token을 안넣고 호출하는 경우
+        //2. 만료된 Token일 경우
+        //3. 적절하지 않은 Token일 경우
 
-        //문열어주기
+        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorizationHeader:{}",authorizationHeader);
+        //1. Token을 안가지고 오는 경우(request할 때 Token 안넣고 호출 or Bearer로 호출 x)
+        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")){
+            log.error("헤더를 가져오는 과정에서 에러가 났습니다. 헤더가 null이거나 잘못되었습니다.");
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //token 분리
+        String token;
+        try {
+            token = authorizationHeader.split(" ")[1];
+        } catch (Exception e) {
+            log.error("token 추출에 실패했습니다.");
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //2. 만료된 Token일 경우
+        if(JwtTokenUtil.isExpired(token,secretKey)){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        //문열어주기 >> 허용
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken("", null, List.of(new SimpleGrantedAuthority("USER")));
         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
--- src/main/java/com/jpa/jpaexercise/utils/JwtTokenUtil.java
+++ src/main/java/com/jpa/jpaexercise/utils/JwtTokenUtil.java
@@ -19,4 +19,12 @@ public class JwtTokenUtil {
                 .signWith(SignatureAlgorithm.HS256,key) //어떤 암호화방식으로 사인할지, key로 잠금
                 .compact();
     }
+    public static boolean isExpired(String token, String secretKey){
+        Date expirationDate = extractClaims(token, secretKey).getExpiration();
+        return expirationDate.before(new Date());
+    }
+
+    private static Claims extractClaims(String token, String key){
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+    }
 }
